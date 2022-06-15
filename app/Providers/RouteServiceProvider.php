fc36a8f40c1bafc45730b63e9e5736c1d97003cf
Furkan <furkan.ozturk.98@hotmail.com>
2022-06-15 21:32:23 +0300

    update headers for cors

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,10 +26,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        header('Access-Control-Allow-Origin:  *');
-        header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-
         $this->configureRateLimiting();
 
         $this->routes(function() {
