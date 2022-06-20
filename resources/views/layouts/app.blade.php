eff1b40eb0d02cd36b32cdd31c06864bc12fa40f
kerimkuscu <Madagaskar12>
2022-06-20 14:24:46 +0300

    feature: add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>TUKOM Electronics</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
