ba282b6fcb820fc35b559b925abfba89fe2bd883
Furkan <furkan.ozturk.98@hotmail.com>
2022-07-25 20:53:38 +0300

    increase importable sub menu count to 7

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -16,21 +16,26 @@ class ProductImport implements ToModel, WithHeadingRow
                 'card_code' => $row['kart_kodu'],
             ],
             [
-                'card_code'      => $row['kart_kodu'],
-                'description'    => $row['aciklama'],
-                'type'           => $row['turu'],
-                'brand'          => $row['marka'],
-                'fiili_stok'     => $row['fiili_stok'],
-                'actual_stock'   => $row['gercek_stok'],
-                'main_unit'      => $row['ana_birim'],
-                'price'          => $row['fiyat_1'],
-                'currency'       => $row['doviz_1'],
-                'group_code'     => $row['grup_kodu'],
-                'special_code_1' => $row['ozel_kod_1'],
-                'special_code_2' => $row['ozel_kod_2'],
-                'special_code_3' => $row['ozel_kod_3'],
-                'special_code_4' => $row['ozel_kod_4'],
-                'special_code_5' => $row['ozel_kod_5'],
+                'card_code'       => $row['kart_kodu'],
+                'description'     => $row['aciklama'],
+                'type'            => $row['turu'],
+                'brand'           => $row['marka'],
+                'fiili_stok'      => $row['fiili_stok'],
+                'actual_stock'    => $row['gercek_stok'],
+                'main_unit'       => $row['ana_birim'],
+                'price'           => $row['fiyat_1'],
+                'currency'        => $row['doviz_1'],
+                'group_code'      => $row['grup_kodu'],
+                'special_code_1'  => $row['ozel_kod_1'],
+                'special_code_2'  => $row['ozel_kod_2'],
+                'special_code_3'  => $row['ozel_kod_3'],
+                'special_code_4'  => $row['ozel_kod_4'],
+                'special_code_5'  => $row['ozel_kod_5'],
+                'special_code_6'  => $row['ozel_kod_6'],
+                'special_code_7'  => $row['ozel_kod_7'],
+                'special_code_8'  => $row['ozel_kod_8'],
+                'special_code_9'  => $row['ozel_kod_9'],
+                'special_code_10' => $row['ozel_kod_10'],
             ]
         );
 
@@ -52,7 +57,7 @@ class ProductImport implements ToModel, WithHeadingRow
 
     public function createSubMenus(int $parentId, array $row)
     {
-        for ($i = 1; $i <= 5; $i++) {
+        for ($i = 1; $i <= 7; $i++) {
             if (empty($row["sub_menu_$i"])) {
                 continue;
             }
